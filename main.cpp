08713de6bb25158627c7ec5dabdf9e15ba273a92
zakkuns <zkuns@mail.csuchico.edu>
2018-02-08 11:27:27 -0800

    Checking if input is upper or lower case

--- main.cpp
+++ main.cpp
@@ -1,24 +1,40 @@
-//Author:
+//Author: Zakary Kuns
 #include<iostream>
 
 using namespace std;
 
 int main()
 {
+        char letter;
 
-  cout<<"What character do you want to know about?\n";
+        cout<<"What character do you want to know about?\n";
 
-  //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
+        cin >> letter;
 
-  //when user's entry is between a-z...
-  cout<<" is a lower case letter!\n";
+        if (letter >= 'A' && letter <= 'Z') { 
+                 //when user's entry is between A-Z..
+                cout<< letter << " is an upper case letter!\n";
+        }
 
-  //in all other cases...
-  cout<<"?! Pssh. What are you talking about?\n";
-  
-  //no matter what they enter...
-  cout<<"The ASCII value is: ";
 
-  return 0;
+
+       else  if (letter >= 'a' && letter <= 'z') {
+                //when user's entry is between a-z...
+                cout<< letter << " is a lower case letter!\n";
+
+        }
+
+        else {
+
+        //in all other cases...
+        cout<<"$?! Pssh. What are you talking about?\n";
+        
+        }
+
+
+
+        //no matter what they enter...
+        cout<<"The ASCII value is: " << (int)letter << std::endl;
+
+        return 0;
 }
